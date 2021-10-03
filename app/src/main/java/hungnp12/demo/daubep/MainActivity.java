6db5dc6c729951174dae18ee91e365438f03e35e
dangnlh <nguyenlehaidang.290698@gmail.com>
2021-10-03 17:55:55 +0700

    sua nut button

---
+++ app/src/main/java/hungnp12/demo/daubep/MainActivity.java
@@ -0,0 +1,78 @@
+package hungnp12.demo.daubep;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Point;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.text.SpannableString;
+import android.text.Spanned;
+import android.text.style.RelativeSizeSpan;
+import android.text.style.StyleSpan;
+import android.view.Display;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
+
+public class MainActivity extends AppCompatActivity{
+
+    private TextView headerHello;
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottom_nav_view);
+        bottomNavigationView.setBackground(null);
+        bottomNavigationView.getMenu().getItem(1).setEnabled(true);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(listener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, new HomeFragment()).commit();
+
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener listener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()){
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_community:
+                            selectedFragment = new CommunityFragment();
+                            break;
+                        default:
+                            selectedFragment = new HomeFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_container, selectedFragment).commit();
+                    return true;
+                }
+            };
+
+    // for forum searching
+    public void ClickToSearch(View view) {
+        Intent intent = new Intent(this,SearchActivity.class);
+        startActivity(intent);
+    }
+
+    public void clicktoCongThuc(View view) {
+        Intent intent = new Intent(this, CongThucNauAnActivity.class);
+        startActivity(intent);
+    }
+}
