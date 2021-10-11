d36e31137e822701571c2dd9121029ba6cf1798c
dangnlh <nguyenlehaidang.290698@gmail.com>
2021-10-11 21:13:53 +0700

    thay doi theo y tui

--- app/src/main/java/hungnp12/demo/daubep/CongThucNauAnActivity.java
+++ app/src/main/java/hungnp12/demo/daubep/CongThucNauAnActivity.java
@@ -23,7 +23,7 @@ public class CongThucNauAnActivity extends AppCompatActivity {
     private EditText email;
     private Button submit ;
     private ChipGroup chipGroup ;
-    private Button btn , btn2 , btn3;
+    private Button btn , btn2 , btn3 ,btn4 , btn5 , btn6;
     Boolean btn2Flag = false ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +33,26 @@ public class CongThucNauAnActivity extends AppCompatActivity {
         btn = findViewById(R.id.button);
         btn2 = findViewById(R.id.button2);
         btn3 = findViewById(R.id.button3) ;
+        btn4 = findViewById(R.id.button4);
+        btn5= findViewById(R.id.button5);
+        btn6 = findViewById(R.id.button6);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (btn2Flag == false){
+                    btn.setTextColor(Color.parseColor("#ef4c29"));
+                    btn2Flag = true ;
+                }else {
+                    btn.setTextColor(Color.parseColor("#000000"));
+                    System.out.println(btn2Flag);
+                    btn2Flag = false ;
+                }
+
+
+            }
+        });
+
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -68,17 +88,54 @@ public class CongThucNauAnActivity extends AppCompatActivity {
 
             }
         });
-        btn.setOnClickListener(new View.OnClickListener() {
+
+        btn4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (btn2Flag == false){
-                    btn.setTextColor(Color.parseColor("#ef4c29"));
+                    btn4.setTextColor(Color.parseColor("#ef4c29"));
                     btn2Flag = true ;
                 }else {
-                    btn.setTextColor(Color.parseColor("#000000"));
+                    btn4.setTextColor(Color.parseColor("#000000"));
                     System.out.println(btn2Flag);
                     btn2Flag = false ;
                 }
+
+
+            }
+        });
+
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (btn2Flag == false){
+                    btn5.setTextColor(Color.parseColor("#ef4c29"));
+                    btn2Flag = true ;
+                }else {
+                    btn5.setTextColor(Color.parseColor("#000000"));
+                    System.out.println(btn2Flag);
+                    btn2Flag = false ;
+                }
+
+
+
+            }
+        });
+
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                if (btn2Flag == false){
+                    btn6.setTextColor(Color.parseColor("#ef4c29"));
+                    btn2Flag = true ;
+                }else {
+                    btn6.setTextColor(Color.parseColor("#000000"));
+                    System.out.println(btn2Flag);
+                    btn2Flag = false ;
+                }
+
 
 
             }
