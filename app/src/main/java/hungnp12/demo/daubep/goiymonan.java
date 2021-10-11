b556ca3f85c1dcb6c26427d0de2f5807a3b7d6aa
dangnlh <nguyenlehaidang.290698@gmail.com>
2021-10-11 14:57:50 +0700

    done

---
+++ app/src/main/java/hungnp12/demo/daubep/goiymonan.java
@@ -0,0 +1,29 @@
+package hungnp12.demo.daubep;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class goiymonan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_goiymonan);
+    }
+
+    public void chitietnauan(View view) {
+        Intent intent = new Intent(this , ChiTietMonAnActivity.class);
+        startActivity(intent);
+    }
+
+    public void clickToContact(View view) {
+        Intent intent = new Intent(this, ContactActivity.class);
+        startActivity(intent);
+    }
+
+
+
+}
